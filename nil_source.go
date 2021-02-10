79824dd09feca3b5e34209040567c33e2eb9adc7
jerluc <me@jerluc.com>
2021-02-09 18:01:29 -0800

    [WIP] Code comments for NilSource

--- nil_source.go
+++ nil_source.go
@@ -6,8 +6,10 @@ import (
 	"github.com/paulmach/orb/geojson"
 )
 
+// NilSource implements the Source interface with no data
 type NilSource struct{}
 
+// GetFeatures returns no data for all requests
 func (n *NilSource) GetFeatures(ctx context.Context, req *TileRequest) (*geojson.FeatureCollection, error) {
 	return geojson.NewFeatureCollection(), nil
 }
